4d820165eff9886cf262ce0e62bf5080ddcf4fa3
vflores <vflores@host.docker.internal>
2023-07-11 18:02:21 -0500

    second commit

--- src/main/java/com/desarrollo/modelo/Conexion.java
+++ src/main/java/com/desarrollo/modelo/Conexion.java
@@ -4,13 +4,13 @@ import java.sql.*;  //importar todo
 
 public class Conexion {
 
-    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/tecnomega?useSSL=false&serverTimezone=UTC";
+    public static final String JDB_URL = "jdbc:mysql://localhost:3306/tecnomega?useSSL=false&serverTimezone=UTC";
     public static final String JDBC_USER = "root";
     public static final String JDBC_PASSWORD = "admin";
 
     //método que retorna el objeto de conexión
     public static Connection getConnection() throws SQLException { //extendiendo la exception
-        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+        return DriverManager.getConnection(JDB_URL, JDBC_USER, JDBC_PASSWORD);
     }
 
     //métodos que cierren los objetos de conexión
